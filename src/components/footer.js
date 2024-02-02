e04110c06b7b055f299216a130d8e5c656b4b5a8
Andriuskevicius <33048776+Andriuskevicius@users.noreply.github.com>
2024-02-02 10:06:24 +0100

    Added footer for desktop

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,118 @@
 import React from 'react'
-
-import Container from './container'
+import { Link } from 'gatsby'
 import * as styles from './footer.module.css'
 
 const Footer = () => (
-  <Container as="footer">
+  <footer className={styles.footer} as="footer">
     <div className={styles.container}>
-      Pagaminta Lietuvoje. Varoma nežabota AI galia.
+      <div className={styles.footerTop}>
+        <Link to="/" className={styles.logoLink}>
+          <div className={styles.logoTextWrap}><span className={styles.footerLogo}>CYBER</span><span>Scalps</span></div>
+        </Link>
+        <div className={styles.subscription}>
+          <input type="email" placeholder="Suvesk email, vistiek negausi nieko" />
+          <button>Prenumeruok naujienlaiškį</button>
+        </div>
+      </div>
+      <div className={styles.footerMiddle}>
+        <ul>
+          <h3 className={styles.title}>Kategorijos:</h3>
+          <li>
+            <Link to="/">
+              <span>Robotika</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>Kripto</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>APPS</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>AI</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>Startuoliai</span>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <h3 className={styles.title}>Hot right now:</h3>
+          <li>
+            <Link to="/">
+              <span>NFT</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>NFT</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>NFT</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>NFT</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>NFT</span>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <h3 className={styles.title}>Apie mus:</h3>
+          <li>
+            <Link to="/">
+              <span>Kontaktai</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>Apie mus</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>Reklama</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>Papildomos rankos</span>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <h3 className={styles.title}>Sekite:</h3>
+          <li>
+            <Link to="/">
+              <span>Threads</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span>Facebook</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.footerBottom}>
+        Pagaminta Lietuvoje. Varoma nežabota AI galia.
+      </div>
     </div>
-  </Container>
+  </footer>
 )
 
 export default Footer
